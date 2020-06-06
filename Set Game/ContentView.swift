180d3f1cf49f2d873fcfd9320fe755461d96faed
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-06 00:41:50 -0500

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Evan Timmons on 6/6/20.
+//  Copyright © 2020 Evan Timmons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
